a7d611b2a0c62782a515bac31e2a42a20e3c0a65
Udith <uditharosha@gmail.com>
2020-04-02 11:39:55 +0530

    Modified to use async-await syntax

--- simple-contact-us-handler/handler.js
+++ simple-contact-us-handler/handler.js
@@ -1,43 +1,49 @@
 let AWS = require('aws-sdk');
 const ddb = new AWS.DynamoDB.DocumentClient();
 const validate = require("validate.js");
-exports.handler = function (event, context, callback) {
-	//validating email and name
-	var constraints = {
-		email: {
-			presence: true,
-			email: true
-		},
-		name: {
-			presence: true,
-			length: {
-				minimum: 1
-			},
-		}
-	};
 
+const constraints = {
+	email: {
+		presence: true,
+		email: true
+	},
+	name: {
+		presence: true,
+		length: {
+			minimum: 1
+		},
+	}
+};
+
+exports.handler = async (event) => {
+
+	//validating email and name
 	let invalid = validate(event, constraints);
 
 	if (!invalid) {
 		let today = new Date().toLocaleDateString();
-		ddb.put({
-			TableName: 'contact_us',
-			Item: {
-				'name': event.name,
-				'email': event.email,
-				'phone': event.phone,
-				'company': event.company,
-				'comment': event.comment,
-				'entryDate': today
-			}
-		}, function (err, data) {
-			if (err) {
-				callback(err, null);
-			} else {
-				callback(null, "Successfully Saved Entry!");
-			}
-		});
+		try {
+			await ddb.put({
+				TableName: 'contact_us',
+				Item: {
+					'name': event.name,
+					'email': event.email,
+					'phone': event.phone,
+					'company': event.company,
+					'comment': event.comment,
+					'entryDate': today
+				}
+			}).promise();
+			console.log("Successfully Saved entry from email:", event.email);
+			return "Successfully Saved Entry!";
+
+		} catch (err) {
+			console.log("Failed to save entry from email:", event.email, err);
+			throw err;
+		}
+
 	} else {
-		callback(JSON.stringify(invalid), null);
+		console.log("Event validation failed", invalid);
+		throw new Error(JSON.stringify(invalid));
 	}
-}
+};
